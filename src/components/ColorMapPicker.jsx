23e5e3765a24aac024d7f40cec3f23fccec52610
Martin Kulka <kulka@metaapp.sk>
2023-05-13 18:54:59 +0200

    added color map changing

--- src/components/ColorMapPicker.jsx
+++ src/components/ColorMapPicker.jsx
@@ -19,20 +19,28 @@ const ColorMapPicker = ({ volume }) => {
   const nv = useContext(NiivueContext);
   const [selected, setSelected] = useState(nv.volumes[volume].colorMap);
 
+  const handleColorChange = (colorMap) => {
+    nv.volumes[volume].colorMap = colorMap;
+    nv.updateGLVolume();
+    
+    setSelected(colorMap);
+  };
+
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={selected} onChange={handleColorChange}>
       <div className="relative">
         <Listbox.Button className="h-10 w-32 cursor-pointer rounded-lg bg-gray-700 py-2 hover:bg-[#414b5a]">
           <p className="text-md font-roboto text-white">{selected}</p>
         </Listbox.Button>
         <Listbox.Options className="absolute mt-1 w-full rounded-md bg-[#414b5a]">
-          {colorMaps
+          {nv.volumes[volume]
+            .colorMaps()
             .filter((map) => map !== selected)
             .map((map) => (
               <Listbox.Option
                 key={map}
                 className={({ active }) =>
-                  `relative flex cursor-default select-none justify-center py-2 ${
+                  `relative flex cursor-pointer select-none justify-center py-2 ${
                     active ? 'bg-sky-400' : 'text-gray-900'
                   }`
                 }
